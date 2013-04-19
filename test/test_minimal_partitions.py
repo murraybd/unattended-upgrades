2d1d6e4a63e79cb0b87f23032d2e16db734be357
Michael Vogt <michael.vogt@ubuntu.com>
2013-04-19 13:44:05 +0200

    calculate the minimal paritions only once and apply them in a sorted order, the old algorithm was re-calculating after each cache.commit() which was rather slow

--- test/test_minimal_partitions.py
+++ test/test_minimal_partitions.py
@@ -7,6 +7,8 @@ import unittest
 
 import unattended_upgrade
 
+apt_pkg.config.set("APT::Architecture", "i386")
+
 
 class LogInstallProgressMock(unattended_upgrade.LogInstallProgress):
 
@@ -23,14 +25,13 @@ class LogInstallProgressMock(unattended_upgrade.LogInstallProgress):
 class TestMinimalPartitions(unittest.TestCase):
 
     def setUp(self):
-        # setup dry-run mode for apt
-        apt_pkg.config.set("Dir::Cache", "/tmp")
+        apt_pkg.config.set("Dir::State::status",
+                           "./root.minimal-upgrade-steps/var/lib/dpkg/status")
         apt_pkg.config.set("Debug::NoLocking", "1")
         apt_pkg.config.set("Debug::pkgDPkgPM", "1")
-        apt_pkg.config.set("Dir::State::extended_states", "./extended_states")
         apt_pkg.config.clear("Dpkg::Post-Invoke")
         apt_pkg.config.clear("Dpkg::Pre-Install-Pkgs")
-        self.cache = apt.Cache()
+        self.cache = apt.Cache(rootdir="./root.minimal-upgrade-steps")
 
     def tearDown(self):
         if os.path.exists("./extended_states"):
@@ -52,6 +53,36 @@ class TestMinimalPartitions(unittest.TestCase):
         # cleanup class data
         LogInstallProgressMock.DATA = []
 
+    def _get_all_upgradable(self):
+        pkgs_to_upgrade = set()
+        self.cache.upgrade(True)
+        for pkg in self.cache.get_changes():
+            pkgs_to_upgrade.add(pkg.name)
+        self.cache.clear()
+        return pkgs_to_upgrade
+
+    def test_find_all_upgrade_partitions(self):
+        # XXX write proper fixtures
+        pkgs_to_upgrade = self._get_all_upgradable()
+        partitions = unattended_upgrade._find_all_upgrade_partitions(
+            self.cache, pkgs_to_upgrade, blacklist=[])
+        seen = set()
+        for part in partitions:
+            seen = seen.union(set(part))
+        self.assertEqual(seen, set(pkgs_to_upgrade))
+
+    def test_upgrade_in_minimal_steps_speedup(self):
+        # XXX write proper fixtures
+        blacklist = []
+        logfile_dpkg = ""
+        pkgs_to_upgrade = self._get_all_upgradable()
+        print(pkgs_to_upgrade)
+        unattended_upgrade.check_changes_for_sanity = lambda *args: True
+        unattended_upgrade.upgrade_in_minimal_steps(
+            self.cache, pkgs_to_upgrade, blacklist, logfile_dpkg)
+
+
 if __name__ == "__main__":
+    #import logging
     #logging.basicConfig(level=logging.DEBUG)
     unittest.main()
