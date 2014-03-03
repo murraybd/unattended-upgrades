20de56f1bd5f9caac9229519b14236f28f56bdad
Michael Vogt <michael.vogt@ubuntu.com>
2014-03-03 23:51:51 +0100

    improve logging

--- test/test_against_real_archive.py
+++ test/test_against_real_archive.py
@@ -23,8 +23,9 @@ class MockOptions():
     def __init__(self, debug=True, dry_run=False):
         self.debug = debug
         self.dry_run = dry_run
-        self.minimal_upgrade_steps = False
+        self.minimal_upgrade_steps = True
         self.verbose = False
+        self.apt_debug = False
 
 
 class TestAgainstRealArchive(unittest.TestCase):
